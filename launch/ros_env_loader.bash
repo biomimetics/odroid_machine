d78269d86d2d7d373e1f3e16156d3175ccf2f814
jlamyi <jlamyi@berkeley.edu>
1999-12-31 17:07:14 -0800

    Updating env_loader to reference exp_ws as active workspace.

--- launch/ros_env_loader.bash
+++ launch/ros_env_loader.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export COOP_SLAM_WORKSPACE=/home/odroid/coop_slam_workspace
+export COOP_SLAM_WORKSPACE=/home/odroid/exp_ws
 source $COOP_SLAM_WORKSPACE/devel/setup.bash
 export ROS_HOSTNAME=$HOSTNAME.local
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$COOP_SLAM_WORKSPACE
